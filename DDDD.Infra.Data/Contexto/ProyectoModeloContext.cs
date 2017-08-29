05452e2e6c99788362e734160013de14321bb5ee
luciano <luciano.zapata314@gmail.com>
2017-08-29 12:30:23 -0300

    Aplicacion base ddd

---
+++ DDDD.Infra.Data/Contexto/ProyectoModeloContext.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Data.Entity;
+namespace DDDD.Infra.Data.Contexto
+{
+    public class ProyectoModeloContext : DbContext
+    {
+        public ProyectoModeloContext() : base("ProyectDDD")
+        {
+
+        }
+    }
+}
